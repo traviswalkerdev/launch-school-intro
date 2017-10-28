01474b8d2e0c506f06f58ec563641856c8a70251
Travis Walker <traviswalkerdev@gmail.com>
2017-10-28 13:23:27 -0700

    Methods folder with say.rb method example

---
+++ methods/say.rb
@@ -0,0 +1,18 @@
+def say
+  # method body goes here
+end
+
+puts "hello"
+puts "hi"
+puts "how are you"
+puts "I'm fine"
+
+
+def say(words = "hello")
+  puts words + "."
+end
+
+say()
+say("hi")
+say("how are you")
+say("I'm fine")
