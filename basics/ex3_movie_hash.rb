d709c8dd26a4cea7eec614f1c740127757beea17
Travis Walker <traviswalkerdev@gmail.com>
2017-10-28 12:10:42 -0700

    Basics ex3 solution

---
+++ basics/ex3_movie_hash.rb
@@ -0,0 +1,9 @@
+movies = {
+  the_shining: 1980,
+  pulp_fiction: 1994,
+  a_clockwork_orange: 1971,
+  once_upon_a_time_in_the_west: 1968,
+  easy_rider: 1969
+}
+
+movies.each { |title, year| puts year }
