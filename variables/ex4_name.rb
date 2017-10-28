0938da2c17b2aae3efe3d365259c5498a7e41ef6
Travis Walker <traviswalkerdev@gmail.com>
2017-10-28 13:02:37 -0700

    Variables ex4 solution

---
+++ variables/ex4_name.rb
@@ -0,0 +1,5 @@
+puts "Please enter your first name:"
+first_name = gets.chomp
+puts "And what is your last name?"
+last_name = gets.chomp
+puts "Okay, so your full name is #{first_name.capitalize} #{last_name.capitalize}."
