0b402f6e293d46723f560b8d24d99f77951b9f55
Travis Walker <traviswalkerdev@gmail.com>
2017-10-29 17:18:14 -0700

    Flow Control folder with conditional example

---
+++ flow_control/conditional.rb
@@ -0,0 +1,39 @@
+puts "Put in a number."
+a = gets.chomp.to_i
+
+if a == 3
+  puts "a is 3"
+elsif a == 4
+  puts "a is 4"
+else
+  puts "a is neither 3, nor 4"
+end
+
+
+################
+# Examples
+if x == 3
+  puts "x is 3"
+end
+
+if x == 3
+  puts "x is 3"
+elsif x == 4
+  puts "x is 4"
+end
+
+if x == 3
+  puts "x is 3"
+else
+  puts "x is NOT 3"
+end
+
+
+# Single line example: must use "then" keyword
+if x == 3 then puts "x is 3" end
+
+# Example with trailing "if"
+puts "x is 3" if x == 3
+
+# Example with "unless" keyword
+puts "x is NOT 3" unless x == 3
