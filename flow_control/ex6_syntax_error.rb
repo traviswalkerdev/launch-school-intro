d59f29be68002439eee88c14c4e63252822dd37a
Travis Walker <traviswalkerdev@gmail.com>
2017-10-29 18:48:41 -0700

    Flow Control ex6 solution

---
+++ flow_control/ex6_syntax_error.rb
@@ -0,0 +1,11 @@
+def equal_to_four(x)
+  if x == 4
+    puts "yup"
+  else
+    puts "nope"
+end
+
+equal_to_four(5)
+
+# Raises syntax error because missing the "end"
+# keyword on the conditional statement (line 6)
