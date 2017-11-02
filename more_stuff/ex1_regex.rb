ca128e662d75b33c1b98530c6fdec81d4bc43ba0
Travis Walker <traviswalkerdev@gmail.com>
2017-11-01 17:13:08 -0700

    More Stuff ex1 solution

---
+++ more_stuff/ex1_regex.rb
@@ -0,0 +1,9 @@
+def word_has_lab?(word)
+  puts word if word =~ /lab/
+end
+
+word_has_lab?("laboratory")
+word_has_lab?("experiment")
+word_has_lab?("Pans Labyrinth")
+word_has_lab?("elaborate")
+word_has_lab?("polar bear")
